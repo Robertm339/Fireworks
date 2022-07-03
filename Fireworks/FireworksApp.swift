9c4be9d884880bccf7a269d1cfff40f795746680
Robertm339 <71312299+Robertm339@users.noreply.github.com>
2022-07-02 17:15:17 -0700

    Initial Commit

---
+++ Fireworks/FireworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireworksApp.swift
+//  Fireworks
+//
+//  Created by Robert Martinez on 7/2/22.
+//
+
+import SwiftUI
+
+@main
+struct FireworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
